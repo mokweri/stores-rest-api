eb1b17c0e88d22c389e74ba86d5cd972c70ffd16
mokweri <mogsobd@gmail.com>
2020-06-11 18:11:21 +0300

    Fixed db

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
-
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
@@ -16,12 +15,6 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
 api = Api(app)
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Store, '/store/<string:name>')
@@ -31,6 +24,5 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-
     db.init_app(app)
     app.run(debug=True)
